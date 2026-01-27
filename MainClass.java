5918b42bf6a064765504c4f172179a98437fa571
Sanjith Saha <it24044@mbstu.ac.bd>
2026-01-27 14:53:37 +0600

    Clean up comments in MainClass.java
    Removed comments for cleaner code.

--- MainClass.java
+++ MainClass.java
@@ -8,10 +8,8 @@ public class MainClass {
         System.out.print("Enter radius: ");
         double r = sc.nextDouble();
 
-        // Create object using constructor
         Circle c = new Circle(r);
 
-        // Print area
         System.out.println("Area = " + c.computeArea());
     }
 }
