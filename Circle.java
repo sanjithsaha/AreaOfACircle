e2e845a4a47f6c1cbf8ab939c70c51aad6c4e055
Sanjith Saha <it24044@mbstu.ac.bd>
2026-01-27 14:53:16 +0600

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,23 +1,21 @@
 public class Circle {
 
-    private double radius;   // private access modifier
+    private double radius;   
 
-    // Constructor
     public Circle(double radius) {
         this.radius = radius;
     }
 
-    // Setter
+   
     public void setRadius(double rad) {
         this.radius = rad;
     }
 
-    // Getter
     public double getRadius() {
         return this.radius;
     }
 
-    // Compute area
+   
     public double computeArea() {
         return radius * radius * Math.PI;
     }
